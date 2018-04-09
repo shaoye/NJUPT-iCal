7cefe801eb4283289de2a4e941e47718d7a5e77d
gaoliang <gaoliangim@gmail.com>
2018-04-09 12:35:44 +0800

    update njupt api

--- courses/courses_server.py
+++ courses/courses_server.py
@@ -9,20 +9,11 @@ def zhengfang():
     password = request.args.get('password')
     try:
         zhengfang = Zhengfang(account=account,password=password)
-        if hasattr(zhengfang,'get_courses'):
-            courses = zhengfang.get_courses()
-        else:
-            courses = zhengfang.get_coursers()
+        courses = zhengfang.get_courses()
     except AuthenticationException:
         return jsonify({'success': False, 'message':'账号或密码有误','coursers':[]})
     except Exception:
         return jsonify({'success': False, 'message':'未知异常','coursers':[]})
-
-    for course in courses:
-        if '单周' in course['week'] or '双周' in course['week']:
-            course['interval'] = 2
-        else:
-            course['interval'] = 1
     return jsonify({'success':True,'courses':courses,'message':''})
 
 if __name__ == "__main__":
